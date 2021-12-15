fcac0c32d2ec51c8cd6e56a215b53bdaf5896254
Awa Diop <nadiop37@gmail.com>
2021-12-15 15:10:36 +0000

    ajout du formulaire

--- boite.js
+++ boite.js
@@ -15,12 +15,19 @@ let liste_idee =
 {
     id:3,
     suggestion:"un truc cool et sympa",
+    statue:true,
+    titre:"titre"
+},
+{
+    id:3,
+    suggestion:"un truc cool et sympa",
     statue:false,
     titre:"titre"
 }
 ]
-
+// recuperation des elemnt du DOM
 var elements = document.getElementById("proposition")
+
 liste_idee.forEach( (idee) => {
 
     let divcarte = document.createElement("div")
@@ -48,5 +55,45 @@ liste_idee.forEach( (idee) => {
     
     
 
+
+})
+
+// REcuperation des info du formulaire
+var valider = document.querySelector(".validation")
+
+valider.addEventListener ('click', (e) => {
+    const inputtitre = document.querySelector(".titre").value
+    const inputsuggestion = document.querySelector(".suggestion").value
+
+const newidee = {
+    id:5,
+    suggestion:inputsuggestion,
+    titre:inputtitre,
+    statue:false
+}
+liste_idee.push(newidee)
+document.querySelector(".form").reset()
+let divcarte = document.createElement("div")
+divcarte.classList.add("card")
+divcarte.style.width = "18rem"
+divcarte.style.display= "inline-block"
+
+const divcardbody = document.createElement("div")
+divcardbody.classList.add("card-body")
+
+
+var p = document.createElement("p");
+p.classList.add("card-text")
+
+var h5 = document.createElement("h5")
+h5.classList.add("card-title")
+
+h5.textContent=newidee.titre
+p.textContent=newidee.suggestion
+
+divcardbody.appendChild(h5)
+divcardbody.appendChild(p)
+divcarte.appendChild(divcardbody)
+elements.appendChild(divcarte)
 
 })
