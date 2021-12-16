c5cc101d512d94228d86e3300a6f3d9d10dc6bd9
Awa Diop <nadiop37@gmail.com>
2021-12-16 12:43:09 +0000

    ajout des cartes propositions

--- boite.js
+++ boite.js
@@ -1,3 +1,7 @@
+let API_URL = "https://bmpiqremuvqolvmpvpfz.supabase.co/rest/v1/boite"
+let API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTA0MzE4OCwiZXhwIjoxOTU0NjE5MTg4fQ.fmJNY-6CG7UhTDb_ImLRI96RGSEc46oJ43bbYAjVOoE'
+
+
 let liste_idee = 
 [
 {
@@ -25,75 +29,95 @@ let liste_idee =
     titre:"titre"
 }
 ]
+
 // recuperation des elemnt du DOM
 var elements = document.getElementById("proposition")
 
-liste_idee.forEach( (idee) => {
+const creerCarte = (idee) => { 
 
-    let divcarte = document.createElement("div")
-    divcarte.classList.add("card")
-    divcarte.style.width = "18rem"
-    divcarte.style.display= "inline-block"
+    const boutonValide ="btn_valide-"+idee.id
+    console.log(boutonValide)
+    const boutonRefuse ="btn_refuser-"+idee.id
+        
+    elements.insertAdjacentHTML(
+        "afterbegin", 
+        
+        `   <div class ="d-flex justify-content-between p-4">
+            <div class="card-body border border-primary shadow">
+                <h5 class="card-title">${idee.titre}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Approuvé/Rejeté</h6>
+                <p class="card-text">${idee.suggestion}</p>
+            <div class="d-flex justify-content-around">
+                <i class="bi bi-check-circle btn "
+                id=${boutonValide}
+                style="color: green ;font-size: 2rem;"></i>                 
+                <i class="bi bi-x-circle btn"
+                id=${boutonValide}
+                style="color: #CE0033;font-size: 2rem;"></i>   
+            </div>
+            </div>
+        </div>
 
-    const divcardbody = document.createElement("div")
-    divcardbody.classList.add("card-body")
+        
 
-
-    var p = document.createElement("p");
-    p.classList.add("card-text")
-
-    var h5 = document.createElement("h5")
-    h5.classList.add("card-title")
-
-    h5.textContent=idee.titre
-    p.textContent=idee.suggestion
-    
-    divcardbody.appendChild(h5)
-    divcardbody.appendChild(p)
-    divcarte.appendChild(divcardbody)
-    elements.appendChild(divcarte)
-    
-    
-
+`        
+    )
+}
+// PARCOURIR LE TABLEAU DES IDEES
+    liste_idee.forEach( (idee) => {
+    creerCarte(idee)
 
 })
-
-// REcuperation des info du formulaire
+// Recuperation des info du formulaire
 var valider = document.querySelector(".validation")
 
 valider.addEventListener ('click', (e) => {
     const inputtitre = document.querySelector(".titre").value
     const inputsuggestion = document.querySelector(".suggestion").value
+    
 
-const newidee = {
-    id:5,
-    suggestion:inputsuggestion,
-    titre:inputtitre,
-    statue:false
-}
-liste_idee.push(newidee)
-document.querySelector(".form").reset()
-let divcarte = document.createElement("div")
-divcarte.classList.add("card")
-divcarte.style.width = "18rem"
-divcarte.style.display= "inline-block"
+    const newidee = {
+        suggestion: inputsuggestion,
+        titre: inputtitre,
+        statue: false
+    }
 
-const divcardbody = document.createElement("div")
-divcardbody.classList.add("card-body")
+    liste_idee.push(newidee)
+    creerCarte(newidee)
 
-
-var p = document.createElement("p");
-p.classList.add("card-text")
-
-var h5 = document.createElement("h5")
-h5.classList.add("card-title")
-
-h5.textContent=newidee.titre
-p.textContent=newidee.suggestion
-
-divcardbody.appendChild(h5)
-divcardbody.appendChild(p)
-divcarte.appendChild(divcardbody)
-elements.appendChild(divcarte)
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+        apikey: API_KEY,
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newidee),
+    })
 
 })
+//  // on vide les champs
+// document.querySelector(".form").reset()
+//  //AJOUT DE LA NOUVELLE IDEE AU NIVEAU DE LA PAGE
+// creerCarte(nouvelleIdee)
+
+// // AFFICHAGE DE LA DES IDEES
+
+// window.addEventListener("DOMContentLoaded", (event) => {
+//     //RECUPERATION DES DONNEES VIA API
+//     fetch(API_URL, {
+//     method: "GET",
+//     headers: {
+//         apikey: API_KEY,
+//     },
+//     }) .then((response) => response.json())
+//     .then((idees) => {
+//     idees.forEach((idee) => {
+//         creerUneCarte(idee)
+//     })
+//     })
+// })
+
+
+
+
+
